d493dc79cf7af2ea76a856cca31468995fb6860c
Himanshu wadhwa <himanshuhw999@gmail.com>
2025-01-28 14:04:34 +0530

    learned api call in NEXT

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,14 @@ import Hello from "../components/hello";
 
 export default function Home() {
   console.log('client/server component')
+  async function serverHit() {
+    const response = await fetch('http://localhost:3000/api');
+    if(response.ok){
+      const json = await response.json();
+      console.log(json.msg);
+    }
+  }
+  serverHit();  
   return (
     <div className="h-screen ">
       <Hello />
